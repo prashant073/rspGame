43c4878db43de9d27a9c102d478fd26c91473f02
prashant073 <prashantkkhanal@gmail.com>
2022-05-06 14:55:36 +0545

    gui added

--- game.js
+++ game.js
@@ -1,108 +1,132 @@
+const users=document.getElementById('user-score');
+const comps=document.getElementById('comp-score');
+const rock=document.getElementById('r');
+const paper=document.getElementById('p');
+const your =document.getElementById('you');
+const oppos =document.getElementById('oppo');
+const scissors=document.getElementById('s');
+const resultDis=document.getElementById('result');
+const lastText=document.getElementById('last');
+let uscore=0;
+let cscore=0;
+//events
+     rock.addEventListener('click',()=>{
+        your.innerHTML="Rock";
+        computerPlay();
+        result("Rock");
 
-
-    //  <h2>First to reach 5 point will be ther winner!</h2>
-    //  <div class =score-board>
-    //      <h3 id="you">You</h3>
-    //      <span id="score_point">0:0</span>
-    //      <h3 id="computer">computer</h3>
-    //  </div>
-    //  <h2 id="result">Rocks beats scisscors. You won!!</h2>
-    //  <button id="r">rock</button>
-    //  <button id="p">paper</button>
-    //  <button id="s">Scisscors</button>
-let userchoice;
-  const r =document.querySelectorAll('button');
-   r.forEach(item=>item.addEventlistener('click',(e)=>{
-    console.log("u clickd me");
-    // console.log(userchoice);  
-    // console.log("hello");
-   }))
-   console.log("hello");
- 
-//  function handle(e){
-//      userchoice=e.target.id;
-//      console.log(userchoice);
-
-//  }
-
-
-
-
-
-
-
-// function computerPlay(){
-//     let ran=['rock','paper','scissors'];
-//    let out=ran[Math.floor(Math.random()*ran.length)];
-//    return out;
-// } 
-
-
-
-// //     //single round play
-// function playRound(playerSelection,computerSelection){
-//      x=playerSelection;
-//      y=computerSelection; 
-//     if(x=='rock' && y=='scissors' || x=='paper' && y=='rock' ||x=='scissors' && y=='paper')
-//      {
-        
-//          return `You Won! ${x} beats ${y}`;
-//      }
-
-//      if(x=='rock' && y=='paper'||x=='paper' && y=='scissors' ||x=='scissors' && y=='rock')
-//      {
-//          return `You Lost! ${y} beats ${x}`;
-//      }
+     });
      
-//      if(x==y)
-//      {
-//          return `It's a Tie! ${x} Tie ${y}`;
-//      }
-     
-//  }
-
-//  //Game function to play 5 rounds,record score and declare winner
-// function game(){
-//     Round =0;
-//     scoreComputer =0;
-//     playerScore=0;
-//     Total = 0;
-//     for(i=1;i<=5;i++){      
-//         const playerSelection=player();
-//         const computerSelection=computerPlay();
-//         const lay=playRound(playerSelection,computerSelection);
-//         console.log(lay);
-//         console.log(Round+=1,"Round");
-
-//     if(x=='rock' && y=='scissors' || x=='paper' && y=='rock' ||x=='scissors' && y=='paper'){ 
-//         console.log(playerScore=playerScore+1,'player');
-//         console.log(scoreComputer=scoreComputer,'computer');
-//      }
-
-//      if(x=='rock' && y=='paper'||x=='paper' && y=='scissors' ||x=='scissors' && y=='rock'){
-//         console.log(playerScore=playerScore,'player');
-//         console.log(scoreComputer=scoreComputer+1,'computer');     
-//      }
-
-//      if(x==y){
-//         console.log(playerScore=playerScore,'player');
-//         console.log(scoreComputer=scoreComputer,'computer'); 
-        
-//      }
+     paper.addEventListener('click',()=>{  
+        your.innerHTML="Paper";
+        computerPlay();
+        result("Paper");
+    });
     
+     scissors.addEventListener('click',()=>{
+        your.innerHTML="Scissors";
+        computerPlay();
+        result("Scissors");
+    });
 
-// } 
-// if(playerScore>scoreComputer)
-//      {
-//          console.log(`You Won! ${playerScore}:${scoreComputer}`);
-//     }
-//     if(playerScore<scoreComputer)
-//      {
-//          console.log(`You Lost! ${playerScore}:${scoreComputer}`);
-//     }
-//     if(playerScore===scoreComputer)
-//     console.log(`Tie Game:: ${playerScore}:${scoreComputer}`);
-// }
-//   game();
-  
+//generate computer choices
+        function computerPlay(){
+            select =['Rock','Scissors','Paper'];
+            computerChoice=select[Math.floor((Math.random()*3))];
+            oppos.innerHTML=computerChoice;
+            return computerChoice;
+        }
 
+//result  and we will be passing some reference to show this beats this..
+       function win(usee,comp){
+           uscore++;
+           resultDis.innerHTML=`You Win!  ${usee} beats ${comp}`;
+        users.innerHTML=uscore;
+        resultDis.style.color="rgb(50, 215, 0)";
+       }
+        
+        function lose(usee,comp){
+            cscore++;
+            resultDis.innerHTML=`You Lost!  ${usee} beats ${comp}`; 
+            comps.innerHTML=cscore;
+            resultDis.style.color="rgb(255, 60, 0)";
+        }
+        function tie(usee,comp){
+            resultDis.innerHTML=`Tie!  ${usee} &  ${comp}`; 
+            resultDis.style.color="yellow";
+        }
+
+    
+       //Function to refreshpage
+       function refreshpage(){
+           window.location.reload(true);
+       }
+
+
+//result display and calling function
+        function result(U){
+        const C=computerPlay();
+        const a = U + C;
+            switch(a){
+                case "RockScissors":
+                case "ScissorsPaper":
+                case "PaperRock":
+                win(U,C);
+                    break;
+
+                case "PaperScissors":
+                case "RockPaper":
+                case "ScissorsRock":
+                lose(U,C); 
+                    break;
+
+                    case "PaperPaper":
+                    case "RockRock":
+                    case "ScissorsScissors":
+                    tie(U,C); 
+                    break;
+            }
+
+            if(uscore===5 ||cscore===5){
+                lastText.innerHTML="Game Over!";
+                lastText.style.color="#cc8813";
+                const rem=document.querySelector('.choices');
+                const rem1 =document.querySelector('.Score-board');
+                const rem3 =document.querySelector('.head');
+                rem.remove();
+                rem1.remove();
+                rem3.remove();
+                    
+        //buttons for refresh  
+        const scoreboard=document.createElement('div');
+        const scorelead=document.createElement('h3');
+       scorelead.innerHTML=`You  ${uscore} : ${cscore} Opponent `;
+       
+       //appending 
+       const bottom =document.getElementById('last messages');    
+       bottom.appendChild(scoreboard);
+       bottom.appendChild(scorelead);
+       
+       if(uscore===5 && cscore<uscore){
+          const my=document.createElement('h2');
+          my.innerHTML=`Winner!!! You're First to score 5`;
+          my.style.color="#16ebe7";
+          bottom.appendChild(my);
+       }
+       else{
+        const my2=document.createElement('h2');
+        my2.innerText=`You Lost!!! Opponent is first to score 5`;
+        my2.style.color="red";
+        bottom.appendChild(my2);
+       }
+       const newgame=document.createElement('button');
+       newgame.innerHTML="Play Again!";
+       newgame.style.border="hidden";
+       bottom.appendChild(newgame);
+       //event 
+       newgame.addEventListener("click",refreshpage);
+       }
+        }
+        
+     
+        
